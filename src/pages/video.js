8906b4727b4ca2135b2848b34cc5f01514e40d09
Meire Mendes <meiremendesrocha@gmail.com>
2023-03-30 02:19:08 -0300

    Incluindo comandos do play

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import "./video.css"
+import React, { useRef, useState } from "react";
+import "./video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
   return (
     <div className="video">
-        Eu sou um componente Video
-        <video 
-            className='video__player'
-            src=''
-        >
-        </video>
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/videos-meire.appspot.com/o/Snapinsta.app_video_584DA83FFE9D8B2941BDD9C80AB966B4_video_dashinit.mp4?alt=media&token=d8cf4d16-65fd-45e8-919a-433cc1692e0e"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
