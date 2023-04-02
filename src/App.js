4ae6314a8cee06720da1843047b95002aa2b3368
Meire Mendes <meiremendesrocha@gmail.com>
2023-04-01 23:00:08 -0300

    from"./config/firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import Video from "./pages/Video";
-import db from "./config/firebase";
+import db from "./ config/firebase";
 import { collection, getDocs } from 'firebase/firestore/lite'
 
 function App() {
